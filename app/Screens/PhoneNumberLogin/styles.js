d1d2377c227f69654a7d27371a58226025657ac8
Muhammad asad <bluehat2023@gmail.com>
2022-04-12 00:23:34 +0500

    Add the Login Screen for phone number users

---
+++ app/Screens/PhoneNumberLogin/styles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  textStyle: {
+    color: '#868686',
+    fontSize: 18,
+    fontWeight: "600"
+  },
+  textCont: {
+    marginTop: 20,
+    marginLeft: 15,
+  },
+  InputCont: {
+    marginVertical: 20, 
+    alignItems: 'center',
+  },
+  btnCont: {
+    marginTop: 40,
+  }
+});
+
+export default styles
